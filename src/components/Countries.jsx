2cd15e02d0bc9ac04e516abb922a9879eb181002
BalThapa <info.balthapa@gmail.com>
2023-09-15 14:58:48 +0300

    added the countries flags and population 2

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,20 +1,29 @@
 import React, { useState } from 'react';
 
-
-import Card from 'react-bootstrap/Card';
+import CountryCard from './CountryCard';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
-import ListGroup from 'react-bootstrap/ListGroup';
 import Row from 'react-bootstrap/Row';
-import { LinkContainer } from 'react-router-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { initializeCountries } from '../features/countries/countriesSlice';
 
 const Countries = () => {
+  const dispatch = useDispatch();
+  const countriesList = useSelector((state) => state.countries.countries);
+  const loading = useSelector((state) => state.countries.isLoading);
+
+  console.log("CountriesList=", countriesList)
+
   const [search, setSearch] = useState('')
 
   console.log("Search: ", search)
 
-  // We will be replacing this with data from our API.
+  useEffect(()=> {
+    dispatch(initializeCountries())
+  },[dispatch]
+  )
   const country = {
     name: {
       common: 'Example Country'
@@ -38,36 +47,13 @@ const Countries = () => {
         </Col>
       </Row>
       <Row xs={2} md={3} lg={4} className=" g-3">
-            <Col className="mt-5">
-              <LinkContainer
-                to={`/countries/${country.name.common}`}
-                state={{ country: country }}
-              >
-                <Card className="h-100">
-                  <Card.Body className="d-flex flex-column">
-                    <Card.Title>{'Single Country Common Name'}</Card.Title>
-                    <Card.Subtitle className="mb-5 text-muted">
-                      {'Single Country Official Name'}
-                    </Card.Subtitle>
-                    <ListGroup
-                      variant="flush"
-                      className="flex-grow-1 justify-content-end"
-                    >
-                      <ListGroup.Item>
-                        <i className="bi bi-translate me-2"></i>
-                      </ListGroup.Item>
-                      <ListGroup.Item>
-                        <i className="bi bi-cash-coin me-2"></i>
-                      </ListGroup.Item>
-
-                      <ListGroup.Item>
-                        <i className="bi bi-people me-2"></i>
-                      </ListGroup.Item>
-                    </ListGroup>
-                  </Card.Body>
-                </Card>
-              </LinkContainer>
-            </Col>
+    
+      {countriesList.map((country)=>{
+        return(
+          <CountryCard country={country}
+          key={ country.name.common } />
+        )
+      })}
       </Row>
     </Container>
   );
