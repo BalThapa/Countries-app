f110ae0990eb74277e46071afa88ab4ed3b37a18
BalThapa <info.balthapa@gmail.com>
2023-09-15 10:26:15 +0300

    initiate new repo

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <div>
+        <h1>Countries</h1>
+        <span>Countries app </span>is a simple React application made in
+        Business College Helsinki lessons. App uses{' '}
+        <a href="https://restcountries.com/">https://restcountries.com/ </a> and{' '}
+        <a href="https://openweathermap.org/">https://openweathermap.org/</a>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
